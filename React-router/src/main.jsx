677cb474877767ec908a9419d1da74a354bdbcf5
Sibtain Raza <khanbhai14063@gmail.com>
2026-04-02 23:37:49 +0530

    Removed currency project and added React Router static page

---
+++ React-router/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import About from './components/About/About.jsx'
+import Github from './components/Github/Github.jsx'
+import User from './components/User/User.jsx'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />} >
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='github' element={<Github />} />
+      <Route path='user/:userid' element={<User />} />
+    </Route>
+  )
+)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router} />
+  </StrictMode>,
+)
