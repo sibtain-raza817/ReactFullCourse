80582d71af3d3465af9f175863a97c4de91fa1f9
Sibtain Raza <khanbhai14063@gmail.com>
2026-04-06 10:06:15 +0530

    Done TodoItem

--- ContextApiLocalStorage/src/App.jsx
+++ ContextApiLocalStorage/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import {TodoProvider} from './context'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 
 function App() {
@@ -20,7 +22,7 @@ function App() {
   }
 
   const toggleComplete = (id) =>{
-    setTodos((prev) => prev.map((prevTodo)=> prevTodo === id ? {...prevTodo, completed : !prevTodo.completed} : prevTodo ))
+    setTodos((prev) => prev.map((prevTodo)=> prevTodo.id === id ? {...prevTodo, completed : !prevTodo.completed} : prevTodo ))
   }
 
   useEffect(()=>{
@@ -40,9 +42,15 @@ function App() {
           <h1 className='text-2xl font-bold text-center mb-8 mt-2'>Manage Your Todos</h1>
           <div className='mb-4'>
             {/* Todo from goes here */}
+            <TodoForm />
           </div>
           <div className='flex flex-wrap gap-y-3'>
             {/*  */}
+            {todos.map((todo)=>(
+              <div key={todo.id} className='w-full'>
+                <TodoItem todo={todo}/>
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- ContextApiLocalStorage/src/components/TodoForm.jsx
+++ ContextApiLocalStorage/src/components/TodoForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import {useTodo} from '../context/TodoContext'
 
 const TodoForm = () => {
   const [todo, setTodo] = useState("")
